97697d1d506aa454ebc9902b95cb78df8c5c46ad
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-15 15:46:15 +0700

    commit6

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -28,26 +28,60 @@ public:
     void adNode()
     {
         int nim;
-        cout << "\"
-    }
-}
-
-
-
-    // Step 2: Assign value to the data fields
+        cout << "";
+         // Step 2: Assign value to the data fields
     newMode->noMhs = nim;
 
     // Step 3: Insert at begining if list is empty or nim is smallest
     if (START == NULL || nim <= START->noMhs)
     {
         if (START != NULL && nim == START->noMhs)
-             cout << "\nDuplicate number not allowed" << endl;
-             return;
-        }
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
             // Step 4 newMode.next = START
             newMode->prev = START;
 
             // Step 5 START.prev = newMode (if START exists)
             if (START != NULL)
-                START->prev = noMhs;
-    
+                START->prev = newMode;
+
+            // Step 6: newMode.prev = NULL
+            newMode->prev = NULL;
+
+            // Step 7: START = newMode
+            START = newMode;
+            return;
+        }
+
+        // insert in between node
+        // Step 8: locate position for insertion
+        Node *Current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next
+        }
+
+        if (current->next != NULL && NIM == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed"  << endl;
+            return 0;
+        }
+
+        //Step 9
+        newMode->next = current->next; // Step 9a
+        newMode->prev = current;       // Step 9b
+        
+        // insert
+        if(current->next != NULL)
+            current->next->prev = noMhs; // step 9c
+        current->next = newNode; // step 9d
+    }
+};
+
+
+
+
+   
+
+ 
