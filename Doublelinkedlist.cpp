36ee8db8d15d032ebd2191e8ee291e40e1156bcd
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-15 16:05:27 +0700

    commit7

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -2,9 +2,9 @@
 #include <string>
 using namespace std;
 
-//nama repository
-//4NIMBLKG_Doublelinkedlist
-//minimal commit 14
+// nama repository
+// 4NIMBLKG_Doublelinkedlist
+// minimal commit 14
 
 class Node
 {
@@ -28,31 +28,35 @@ public:
     void adNode()
     {
         int nim;
-        cout << "";
-         // Step 2: Assign value to the data fields
-    newMode->noMhs = nim;
+        cout << "\nEnter the roll number of the student:";
+        cin >> nim;
 
-    // Step 3: Insert at begining if list is empty or nim is smallest
-    if (START == NULL || nim <= START->noMhs)
-    {
-        if (START != NULL && nim == START->noMhs)
+        // Step 1
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at begining if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
                 cout << "\nDuplicate number not allowed" << endl;
-                return;
-            }
-            // Step 4 newMode.next = START
-            newMode->prev = START;
-
-            // Step 5 START.prev = newMode (if START exists)
-            if (START != NULL)
-                START->prev = newMode;
-
-            // Step 6: newMode.prev = NULL
-            newMode->prev = NULL;
-
-            // Step 7: START = newMode
-            START = newMode;
             return;
         }
+        // Step 4 newMode.next = START
+        newNode->prev = START;
+
+        // Step 5 START.prev = newMode (if START exists)
+        if (START != NULL)
+            START->prev = newNode;
+
+        // Step 6: newMode.prev = NULL
+        newNode->prev = NULL;
+
+        // Step 7: START = newMode
+        START = newNode;
+        return;
 
         // insert in between node
         // Step 8: locate position for insertion
@@ -64,24 +68,35 @@ public:
 
         if (current->next != NULL && NIM == current->next->noMhs)
         {
-            cout << "\nDuplicate roll number not allowed"  << endl;
+            cout << "\nDuplicate roll number not allowed" << endl;
             return 0;
         }
 
-        //Step 9
-        newMode->next = current->next; // Step 9a
-        newMode->prev = current;       // Step 9b
-        
+        // Step 9
+        newNode->next = current->next; // Step 9a
+        newNode->prev = current;       // Step 9b
+
         // insert
-        if(current->next != NULL)
+        if (current->next != NULL)
             current->next->prev = noMhs; // step 9c
-        current->next = newNode; // step 9d
+        current->next = newNode;         // step 9d
     }
-};
+}
 
+void
+hapus()
+{
+    if (START = NULL)
+    {
+        cout << "\nList is empty" << endl;
+        return;
+    }
 
+    cout << "\nMasukan nim yang akan dideleted";
+    int rollNo;
+    cin >> rollNo;
 
-
-   
-
- 
+    // Step 2: if node is at the begining
+}
+}
+;
